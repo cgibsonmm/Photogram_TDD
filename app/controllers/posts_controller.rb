42f7f138aff22c609241d43575057d28ecad1968
Corey Gibson <cgibsonmm@gmail.com>
2018-02-01 19:41:11 -0500

    added post edit/ update with basic validation: passing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,16 +10,31 @@ class PostsController < ApplicationController
   def create
     @post = Post.create(post_params)
     if @post.save
-      flash.now[:success] = "Your post has been created."
+      flash[:success] = "Your post has been created."
       redirect_to @post
     else
-      flash.now[:alert] = "You need a photo to create post!"
+      flash[:alert] = "You need a photo to create post!"
       render 'new'
     end
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = "Post updated!"
+      redirect_to @post
+    else
+      flash[:alert] = "Error updating post!"
+      render 'edit'
+    end
   end
 
   private
---
+++ spec/features/edit_post_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature 'Edit post' do
+  background do
+    post = create(:post, caption: 'Opps!')
+
+    visit '/'
+    find(:xpath, "//a[contains(@href, 'posts/1')]").click
+    click_link 'Edit Post'
+  end
+
+  scenario 'can edit a post' do
+    fill_in 'Caption', with: 'You were not meant to see that!'
+    click_button 'Update Post'
+
+    expect(page).to have_content('Post updated!')
+    expect(page).to have_content('You were not meant to see that!')
+  end
+
+  context 'Error editing post' do
+    it 'requires a photo' do
+      attach_file('Image', 'spec/files/coffee.zip')
+      click_button 'Update Post'
+
+      expect(page).to have_content('Error updating post!')
+    end
+  end
+end
