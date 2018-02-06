36d42d57decae39ab0caf9f6b594b578f0ae8843
Corey Gibson <cgibsonmm@gmail.com>
2018-02-06 18:52:29 -0500

    finished owner vaidation for adding and editing posts:passing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :post_owner, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all.order('created_at DESC')
@@ -21,15 +23,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       flash[:success] = "Post updated!"
       redirect_to @post
@@ -40,7 +39,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
     flash[:success] = 'Post deleted!'
@@ -50,5 +48,16 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_owner
+    unless current_user.id == @post.user.id
+      flash[:alert] = 'You must own this post to edit it!'
+      redirect_to root_path
+    end
   end
 end
--- spec/features/edit_post_spec.rb
+++ spec/features/edit_post_spec.rb
@@ -3,28 +3,48 @@ require 'rails_helper'
 RSpec.feature 'Edit post' do
   background do
     user = create(:user)
-    post = create(:post, caption: 'Opps!', user_id: 1)
+    user_2 = create(:user, email: 'test2@email.com', username: 'testuser2',
+                    password: 'testpassword', id: 2)
+    post1 = create(:post, caption: 'Opps!', user_id: 1)
+    post2 = create(:post, caption: 'User 2 post', user_id: 2)
 
     sign_in_with user
+  end
 
+  def visit_my_post
     find(:xpath, "//a[contains(@href, 'posts/1')]").click
     click_link 'Edit Post'
   end
 
-  scenario 'can edit a post' do
-    fill_in 'Caption', with: 'You were not meant to see that!'
-    click_button 'Update Post'
+  context 'success editing post' do
+    scenario 'owner can edit a post' do
+      visit_my_post
 
-    expect(page).to have_content('Post updated!')
-    expect(page).to have_content('You were not meant to see that!')
+      fill_in 'Caption', with: 'You were not meant to see that!'
+      click_button 'Update Post'
+
+      expect(page).to have_content('Post updated!')
+      expect(page).to have_content('You were not meant to see that!')
+    end
   end
 
   context 'Error editing post' do
     it 'requires a photo' do
+      visit_my_post
       attach_file('Image', 'spec/files/coffee.zip')
       click_button 'Update Post'
 
       expect(page).to have_content('Error updating post!')
+    end
+
+    it 'only owner can update post via the show path' do
+      find(:xpath, "//a[contains(@href, 'posts/2')]").click
+      expect(page).not_to have_content("Edit Post")
+    end
+
+    it 'only owner can edit a post' do
+      visit 'posts/2/edit'
+      expect(page).to have_content('You must own this post to edit it!')
     end
   end
 end
