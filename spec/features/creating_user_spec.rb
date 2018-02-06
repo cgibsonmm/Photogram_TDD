8a6c7f23d40a331926b943f6023ea7f7785eedc9
Corey Gibson <cgibsonmm@gmail.com>
2018-02-06 16:11:59 -0500

    finished user registration validations: passing

--- spec/features/creating_user_spec.rb
+++ spec/features/creating_user_spec.rb
@@ -16,4 +16,35 @@ RSpec.feature 'Creating a new user' do
     click_button 'Sign up'
     expect(page).to have_content('Welcome! You have signed up successfully.')
   end
+
+  context "can't create user" do
+    scenario 'must have a username' do
+      fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+      fill_in 'user_password', with: 'supersecret'
+      fill_in 'user_password_confirmation', with: 'supersecret'
+
+      click_button 'Sign up'
+      expect(page).to have_content("can't be blank")
+    end
+
+    scenario 'username must be more than 4 chars' do
+      fill_in 'User name', with: 'sxy'
+      fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+      fill_in 'user_password', with: 'supersecret'
+      fill_in 'user_password_confirmation', with: 'supersecret'
+
+      click_button 'Sign up'
+      expect(page).to have_content('minimum is 4 characters')
+    end
+
+    scenario 'username must be less than 12 chars' do
+      fill_in 'User name', with: 's' * 13
+      fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+      fill_in 'user_password', with: 'supersecret'
+      fill_in 'user_password_confirmation', with: 'supersecret'
+
+      click_button 'Sign up'
+      expect(page).to have_content('maximum is 12 characters')
+    end
+  end
 end
